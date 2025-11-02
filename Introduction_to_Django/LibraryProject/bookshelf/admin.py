9706528cd8ac50082d5f7d0ad2a78abce78a182f
Amer <amermmhamad@gmail.com>
2025-11-02 23:55:15 +0200

    asfasff

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Columns in the changelist
+    list_display = ("title", "author", "publication_year")
+    # Quick filters on the right
+    list_filter = ("publication_year",)
+    # Top search box (case-insensitive contains by default)
+    search_fields = ("title", "author")
+    # Nice-to-have defaults
+    ordering = ("title",)
+    list_per_page = 25
